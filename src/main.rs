85fae85fe3ba7ac966cfb33ffe348ef031557a2b
Johannes Schneider <johannes.schneider03@sap.com>
2023-12-04 15:29:40 +0100

    feat: implement day1-challenge1

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::{arg, ArgMatches, Command};
+
+mod day1;
+
+fn cli() -> Command {
+    Command::new("aoc")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("day1")
+                .arg(arg!(<FILE> "The input file for the challenge."))
+                .arg_required_else_help(true)
+        )
+}
+
+fn day1(sub_matches: &ArgMatches) {
+    let file = sub_matches.get_one::<String>("FILE").expect("required arg");
+    let maybe_result = day1::day1(file);
+    println!("Result of Day1 challenge: {}", maybe_result.unwrap());
+}
+
+
+fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("day1", sub_matches)) => {
+            day1(sub_matches);
+        }
+        _ => unreachable!()
+    }
+}
